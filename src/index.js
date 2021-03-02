aef672c3bb10522f36a904f9093a55deea2c5f7d
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-03-02 13:19:13 -0600

    2nd

--- src/index.js
+++ src/index.js
@@ -1,32 +1,68 @@
 // write your code here
-
+const ramens = document.querySelector("div#ramen-menu")
+    
 
 function menu() {
     fetch("http://localhost:3000/ramens")
         .then (response => response.json())
         .then (data => {
             data.forEach(ramensObj => {
-                renderRamenItems(ramensObj)
+                const div = document.createElement("div")
+                div.classList.add('imagePic')
+                div.dataset.id = ramensObj.id
+                div.innerHTML = ` <div id="ramen-deta">
+                                 <img class="detail-image" src="${ramensObj.image}" alt="Insert Name Here" />
+                                <h2 class="name">${ramensObj.name}</h2>
+                                <h3 class="restaurant">${ramensObj.restaurant}"</h3>
+                                </div>`
+            
+                ramens.append(div)
+                console.log(ramens)
+                //renderRamenItems()
+                console.log(ramensObj.id)
             })
-            console.log(image)
-            console.log(data.name)
+            //const ramens = document.querySelector("div#ramen-menu")
+             ramens.append(div)
+             console.log(ramens)
+             ramens.addEventListener('click',function(){
+
+             })
+            
+   
+           
          })        
-
+       
 }
 
-function renderRamenItems(ramensObj){
-    const div = document.createElement("div")
-    div.classList.add('imagePic')
-    div.dataset.id = ramensObj.id
+// function renderRamenItems(ramensObj){
+//     const div = document.createElement("div")
+//     div.classList.add('imagePic')
+//     div.dataset.id = ramensObj.id
+   
+//     div.innerHTML = ` <div id="ramen-deta">
+//             <img class="detail-image" src="${ramensObj.image}" alt="Insert Name Here" />
+//                 <h2 class="name">${ramensObj.name}</h2>
+//                 <h3 class="restaurant">${ramensObj.restaurant}"</h3>
+//             </div>`
 
-    div.innerHTML = ` <div id="ramen-deta">
-            <img class="detail-image" src="${ramensObj.image}" alt="Insert Name Here" />
-                <h2 class="name">${ramensObj.name}</h2>
-                <h3 class="restaurant">${ramensObj.restaurant}"</h3>
-            </div>`
-            const ramens = document.querySelector("div#ramen-menu")
-            ramens.append(div)
+//             const ramens = document.querySelector("div#ramen-menu")
+//              ramens.append(div)
+//             // console.log(ramens)
+//             // console.log(ramensObj.id)
+            
+// }
+
+const menuToo = document.querySelector('div#ramen-detail')
+const rate = document.querySelector('#ramen-rating')
+console.log(rate)
+console.log(menuToo)
+
+function dets() {
+    fetch('http://localhost:3000/ramens/${data.id}')
 }
+
+     
+    
     // "id": 3,
     // "name": "Nirvana Shiromaru",
     // "restaurant": "Ippudo",
